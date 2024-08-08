d1a36909ade01a61e8af643a1c3d424e99eb4ffe
sam <sam990126@gmail.com>
2024-08-08 03:43:43 -0700

    fix linux can't create process bug

--- pythonServer/server.py
+++ pythonServer/server.py
@@ -9,13 +9,14 @@ from aiortc.rtcconfiguration import RTCIceServer,RTCConfiguration
 from aiortc.contrib.media import MediaPlayer, MediaRelay, MediaBlackhole
 from aiortc.rtcrtpsender import RTCRtpSender
 from aiortc.sdp import candidate_from_sdp
-
+import sys
 async def registerServer(ws,serverId):
     await ws.send(json.dumps({"operation":"registerPythonServer","serverId":serverId}))
 
 def createProcess(uploaderId):
-    # subprocess.Popen(["sys.executable","streamProcess.py","-uploaderId",uploaderId])
-    subprocess.Popen(["python","streamProcess.py","-uploaderId",uploaderId],shell=True)
+    subprocess.Popen([sys.executable,"streamProcess.py","-uploaderId",uploaderId])
+    # subprocess.Popen(["python","streamProcess.py","-uploaderId",uploaderId],shell=True)
+
 async def websocketClient(serverId):
     async with websockets.connect("ws://localhost:8765") as websocket:
         await registerServer(websocket,serverId)
